d280978d0e4d2871665281c39b2a5b6565d7fba2
Tobias Klöpper <tobiaskloepper1@web.de>
2021-01-01 19:47:04 +0100

    added validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 4..20, message: "Please choose a name between 6 and 20 characters!"}
+  validates :email, uniqueness: true
 end
